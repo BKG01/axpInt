d69daeb36c6586fcf1a5fbf261154711db40b4b6
Ruvimbo <rimairuvi@gmail.com>
2024-03-01 20:06:16 +0200

    app modifications

--- school-management-system/src/App.tsx
+++ school-management-system/src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import './App.css';
 import SchoolForm from './SchoolForm';
-
+import './SchoolForm.css';
 const App: React.FC = () => {
     return (
         <div className="App">
